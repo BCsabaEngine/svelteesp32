9f5d33d7c7a6820007fb634009468e79290922b4
Balázs Csaba <csaba@balazs-magyar.hu>
2024-02-17 15:38:19 +0100

    feat: etag

--- src/cppCode.ts
+++ src/cppCode.ts
@@ -6,6 +6,7 @@ export type cppCodeSource = {
   mime: string;
   content: Buffer;
   isGzip: boolean;
+  md5: string;
 };
 
 const replaceAll = (s: string, from: string, to: string) => {
@@ -13,12 +14,20 @@ const replaceAll = (s: string, from: string, to: string) => {
   return s;
 };
 
+const blockTemplateETag = `if (request->hasHeader("If-None-Match") && request->header("If-None-Match") == String(etag$index$)) {
+      PsychicResponse response304(request);
+      response304.setCode(304);
+      return response304.send();
+    }
+`;
 const blockTemplate = `
   $default$server->on("/$filename$", HTTP_GET, [](PsychicRequest * request)
   {
+    ${cmdLine.etag ? blockTemplateETag : ''}
     PsychicResponse response(request);
     response.setContentType("$mime$");
     response.addHeader("Content-Encoding", "$encoding$");
+    ${cmdLine.etag ? `response.addHeader("ETag", etag$index$);` : ''}
     response.setContent(data$index$, sizeof(data$index$));
     return response.send();
   });
@@ -36,13 +45,13 @@ const getCppBlock = (source: cppCodeSource): string => {
   return result;
 };
 
-const fileTemplate =
-  `//cmdline:  $commandline$
+const fileTemplate = `//cmdline:  $commandline$
 //created:  $now$
 //files:    $filecount$
 //memory:   $filesize$
 
 $filedataarrays$
+$hashaarrays$
 
 void $method$(PsychicHttpServer * server) {
 $code$
@@ -50,10 +59,12 @@ $code$
 
 export const getCppCode = (sources: cppCodeSource[]) => {
   const fileDataArrays: string[] = [];
+  const hashArrays: string[] = [];
   const blocks: string[] = [];
   for (const source of sources) {
     const bytesString = [...source.content].map((v) => `0x${v.toString(16)}`).join(', ');
     fileDataArrays.push(`const uint8_t data${source.index}[${source.content.length}] = {${bytesString}};`);
+    hashArrays.push(`const char * etag${source.index} = "${source.md5}";`);
     blocks.push(getCppBlock(source));
   }
 
@@ -61,9 +72,14 @@ export const getCppCode = (sources: cppCodeSource[]) => {
   result = replaceAll(result, '$commandline$', process.argv.slice(2).join(' '));
   result = replaceAll(result, '$now$', `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`);
   result = replaceAll(result, '$filecount$', sources.length.toString());
-  result = replaceAll(result, '$filesize$', sources.reduce((previous, current) => previous + current.content.length, 0).toString());
+  result = replaceAll(
+    result,
+    '$filesize$',
+    sources.reduce((previous, current) => previous + current.content.length, 0).toString()
+  );
 
   result = replaceAll(result, '$filedataarrays$', fileDataArrays.join('\n'));
+  result = replaceAll(result, '$hashaarrays$', cmdLine.etag ? hashArrays.join('\n') : '');
   result = replaceAll(result, '$method$', cmdLine.espmethod);
   result = replaceAll(result, '$code$', blocks.join(''));
   return result;
