9023094467c4ce345a893698de68c029b8b6b305
Balázs Csaba <csaba@balazs-magyar.hu>
2024-08-02 22:40:51 +0200

    chore: new eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,66 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unicorn from 'eslint-plugin-unicorn';
+import globals from 'globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  {
+    ignores: [
+      '**/.DS_Store',
+      '**/node_modules',
+      '**/bin',
+      '**/dist',
+      '**/svelte',
+      '**/.env',
+      '**/.env.*',
+      '!**/.env.example',
+      '**/pnpm-lock.yaml',
+      '**/package-lock.json',
+      '**/yarn.lock'
+    ]
+  },
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:unicorn/all', 'prettier'),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      'simple-import-sort': simpleImportSort,
+      unicorn
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module'
+    },
+
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unicorn/filename-case': 'off',
+      'unicorn/no-process-exit': 'off',
+      'unicorn/switch-case-braces': 'off',
+      'unicorn/no-array-reduce': 'off',
+      'no-alert': 'error',
+      'no-debugger': 'error'
+    }
+  }
+];
