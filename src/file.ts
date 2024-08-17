7f1a4b493cb083b8f030f97562f1cb434752db0b
BCsabaEngine <csaba@balazs-magyar.hu>
2024-08-17 12:09:02 +0200

    feat: detect precompressed files

--- src/file.ts
+++ src/file.ts
@@ -4,7 +4,18 @@ import { globSync } from 'glob';
 
 import { cmdLine } from './commandLine';
 
-export const getFiles = (): string[] =>
-  globSync('**/*', { cwd: cmdLine.sourcepath, nodir: true })
-    .filter((filename) => !['.gz', '.brottli'].includes(path.extname(filename)))
-    .sort();
+export const getFiles = (): string[] => {
+  let files = globSync('**/*', { cwd: cmdLine.sourcepath, nodir: true });
+  files = files.filter((filename) => {
+    const extension = path.extname(filename);
+    if (['.gz', '.brottli', '.br'].includes(extension)) {
+      const original = filename.slice(0, -1 * extension.length);
+      if (files.includes(original)) {
+        console.log(`Skip because ${filename} is perhaps a compressed version of ${original}`);
+        return false;
+      }
+    }
+    return true;
+  });
+  return files.sort();
+};
