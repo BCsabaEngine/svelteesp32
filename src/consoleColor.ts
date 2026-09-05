da74d2bf4eeb31be0b87dd0db4053ea5789817bd
BCsabaEngine <csaba@balazs-magyar.hu>
2026-09-05 08:45:18 +0200

    feat: consoleColor in non tty mode

--- src/consoleColor.ts
+++ src/consoleColor.ts
@@ -1,7 +1,9 @@
-export const greenLog = (s: string): string => `\u{1B}[32m${s}\u{1B}[0m`;
+const isUseColor = !process.env['NO_COLOR'] && (!!process.env['FORCE_COLOR'] || !!process.stdout.isTTY);
 
-export const yellowLog = (s: string): string => `\u{1B}[33m${s}\u{1B}[0m`;
+export const greenLog = (s: string): string => (isUseColor ? `\u{1B}[32m${s}\u{1B}[0m` : s);
 
-export const redLog = (s: string): string => `\u{1B}[31m${s}\u{1B}[0m`;
+export const yellowLog = (s: string): string => (isUseColor ? `\u{1B}[33m${s}\u{1B}[0m` : s);
 
-export const cyanLog = (s: string): string => `\u{1B}[36m${s}\u{1B}[0m`;
+export const redLog = (s: string): string => (isUseColor ? `\u{1B}[31m${s}\u{1B}[0m` : s);
+
+export const cyanLog = (s: string): string => (isUseColor ? `\u{1B}[36m${s}\u{1B}[0m` : s);
